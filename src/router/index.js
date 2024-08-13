11e575ed8159f49b66056c5f37019d84f7f59e46
LuluuHuang <yuruh03@gmail.com>
2024-08-13 17:52:59 +0800

    更新路由

--- src/router/index.js
+++ src/router/index.js
@@ -1,39 +1,39 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 import SearchPriceView from'../views/SearchPriceView.vue'
 import PrescriptionView from '../views/PrescriptionView.vue'
-import HomeView from '../views/HomeView.vue'
+// import HomeView from '../views/HomeView.vue'
 import AddnDeleteView from '../views/AddnDeleteView.vue'
 import SearchPresView from '../views/SearchPresView.vue'
 import MainLayouotView from '../views/MainLayouotView.vue'
-import LoginView from '../views/LoginView.vue'
-import { useUserStore } from '@/stores/user'
+// import LoginView from '../views/LoginView.vue'
 // import { useUserStore } from '../stores/user'
 
 
 const router = createRouter({
   history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
+    // {
+    //   path: '/',
+    //   redirect: '/login',  // Redirect root path to /login
+    //   meta: { requiresAuth: false },
+    // },
+    // {
+    //   path:'/login',
+    //   name:'Login',
+    //   component:LoginView,
+    //   meta: { requiresAuth: false },
+    // },
     {
-      path: '/',
-      redirect: '/login',  // Redirect root path to /login
-      meta: { requiresAuth: false },
-    },
-    {
-      path:'/login',
-      name:'Login',
-      component:LoginView,
-      meta: { requiresAuth: false },
-    },
-    {
-      path:'/main',
+      path:'/',
+      redirect: '/search',
       component: MainLayouotView,
       children:[
-        {
-          path:'home',
-          name:'Home',
-          component:HomeView,
-          meta: { requiresAuth: true },
-        },
+        // {
+        //   path:'home',
+        //   name:'Home',
+        //   component:HomeView,
+        //   meta: { requiresAuth: true },
+        // },
         {
           path:'search',
           name:'Search',
@@ -63,24 +63,24 @@ const router = createRouter({
   ]
 })
 
-router.beforeEach((to,from,next)=>{
-  const store = useUserStore();
-  // const isLogIn = store.user;
-  // if(isLogIn && to.path === '/login'){
-  //   console.log(1);
-  //   next('/main/search');
-  // }else if(!isLogIn && to.meta.requiresAuth){
-  //   console.log(2);
-  //   next('/login');
-  // }else{
-  //   console.log(3);
-  //   next();
-  // }
-  if(store.user != ''){
-    next();
-  }else{
-    next('/login')
-  }
-})
+// router.beforeEach((to,from,next)=>{
+//   const store = useUserStore();
+//   // const isLogIn = store.user;
+//   // if(isLogIn && to.path === '/login'){
+//   //   console.log(1);
+//   //   next('/main/search');
+//   // }else if(!isLogIn && to.meta.requiresAuth){
+//   //   console.log(2);
+//   //   next('/login');
+//   // }else{
+//   //   console.log(3);
+//   //   next();
+//   // }
+//   if(store.user != ''){
+//     next();
+//   }else{
+//     next('/login')
+//   }
+// })
 
 export default router
